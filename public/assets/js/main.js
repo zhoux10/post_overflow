858d8d0920c5e582da6159dd6d23ce898b2f939a
sowjanya <sowju19@gmail.com>
2015-02-25 15:59:39 -0800

    updated the module pattern

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -23,5 +23,30 @@
     $scope.modulo = function() {
       return $scope.firstNumber % $scope.secondNumber;
     }
-  }]); 
+  }]);
+
+  app.controller('AlternateCalculateController',function() {
+    this.firstNumber = 1;
+    this.secondNumber = 1;
+
+    this.add = function() {
+      return this.firstNumber + this.secondNumber;
+    }
+
+    this.subtract = function() {
+      return this.firstNumber - this.secondNumber;
+    }
+
+    this.multiply = function() {
+      return this.firstNumber * this.secondNumber;
+    }
+   
+    this.divide = function() {
+      return this.firstNumber / this.secondNumber;
+    }
+
+    this.modulo = function() {
+      return this.firstNumber % this.secondNumber;
+    }
+  }); 
 })();
