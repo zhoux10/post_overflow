00ce1a462c2cbd4dd082c93f0898a2ad2ab89cd9
Sherry Zhou <5643f56c@opayq.com>
2015-02-26 22:25:00 -0800

    Add capitalize

--- public/assets/js/student.js
+++ public/assets/js/student.js
@@ -2,37 +2,55 @@
   var hello = angular.module('hello', []);
   hello.controller("stringGameController", ['$scope', function($scope) {
   // body...
-    $scope.firstName = 'Kerry';
-    $scope.lastName = 'Rosado';
+    $scope.firstName = '';
+    $scope.lastName = '';
     $scope.firstHidden = false;
     $scope.firstButtonText = "Hide";
 
-    $scope.add = function() {
-      return $scope.firstName + ' ' + $scope.lastName;
+    $scope.reading = function() {
+      if ($scope.firstName && $scope.lastName) {
+        return $scope.capitalize($scope.firstName) + ' ' + $scope.capitalize($scope.lastName);
+      }
+    };
+
+    $scope.forms = function() {
+      if ($scope.firstName && $scope.lastName) {
+        return $scope.capitalize($scope.lastName) + ', ' + $scope.capitalize($scope.firstName);
+      }
     };
 
     $scope.hideFirst = function () {
       $scope.firstHidden = !$scope.firstHidden;
       if ($scope.firstHidden) {
-        $scope.firstButtonText = "Hide";
-      } else {
         $scope.firstButtonText = "Show";
+      } else {
+        $scope.firstButtonText = "Hide";
       }
+    };
+
+    $scope.capitalize = function (string) {
+      return string[0].toUpperCase() + string.slice(1);
     };
   }]);
 
   hello.controller('stringGameControllerNew', ["$scope", function ($scope) {
-    $scope.className = "Biology";
-    $scope.credits = 1;
+    $scope.className = "";
+    $scope.credits = null;
     $scope.secondHidden = false;
     $scope.secondButtonText = "Hide";
 
     $scope.hideSecond = function () {
       $scope.secondHidden = !$scope.secondHidden;
       if ($scope.secondHidden) {
-        $scope.secondButtonText = "Hide";
-      } else {
         $scope.secondButtonText = "Show";
+      } else {
+        $scope.secondButtonText = "Hide";
+      }
+    };
+
+    $scope.classParse = function () {
+      if ($scope.className && $scope.credits) {
+        return $scope.className + " has " + $scope.credits;
       }
     };
   }]);
