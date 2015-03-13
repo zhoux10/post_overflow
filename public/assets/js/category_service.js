cc325014f233f76ba496ba5df1550b3598d38ed9
Sherry Zhou <5643f56c@opayq.com>
2015-03-12 20:10:38 -0700

    Update formatting

--- public/assets/js/category_service.js
+++ public/assets/js/category_service.js
@@ -2,16 +2,16 @@
   var app = angular.module('categoryServiceModule', []);
   app.factory('categoryService',['$location', '$http', function($location, $http) {
     var myServiceInstance = {};
-    var categoriesUrl = '/categories.json'
-    myServiceInstance.cats = []; 
+    var categoriesUrl = '/categories.json';
+    myServiceInstance.cats = [];
     $http({
       method: 'get',
-      url:  categoriesUrl,           
+      url:  categoriesUrl,
       headers: { 'Content-Type': 'application/json' }
       }).success(function(data) {
         myServiceInstance.cats = data;
       });
-    
+
     return myServiceInstance;
   }]);
 })();
